1b28e59e12529d5fb92a5749fbe090e443d554be
JeremyElias <jereelias99@gmail.com>
2024-11-13 19:51:28 -0300

    Inicialization proyecy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import auth from 'auth-astro';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth(), tailwind()],
+  output: 'server',
+  adapter: vercel()
+});
