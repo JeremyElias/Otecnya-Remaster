ca313ceba7a5f4dca04834ff341a9ae331539c2a
JeremyElias <jereelias99@gmail.com>
2024-11-18 19:51:20 -0300

    style index for projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import auth from 'auth-astro';
-
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), tailwind()],
+  integrations: [auth(), tailwind(), react()],
   output: 'server',
   adapter: vercel()
 });
